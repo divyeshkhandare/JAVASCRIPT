329dc414a4e8d1927d67bfdd23a8887fd18d0cd3
Divyesh Khandare <divyeshkhandare@gmail.com>
2024-07-18 10:28:46 +0530

    Js

--- PR-8/1.js
+++ PR-8/1.js
@@ -2,7 +2,7 @@
 
 let array = ["RED", "AND", "WHITE"];
 for (let i = 0; i < array.length; i++) {
-  if (array[i] == "AND") {
+  if (i % 2 !== 0) {
     console.log(array[i].toLowerCase());
   } else {
     console.log(array[i]);
