ee8591e3ecb389886610073ead591308de4d227e
divyeshkhandare <divyeshkhandare@gamil.com>
2024-07-23 15:34:13 +0530

    js

--- EXAM-4/index.js
+++ EXAM-4/index.js
@@ -44,19 +44,82 @@ const content = () => {
 };
 
 const content2 = () => {
+  let fee = 0;
+  for (let i = 0; i < users.length; i++) {
+    if ((users[i].fees = users[i].fees1)) {
+      fee += Number(users[i].fees1);
+    }
+  }
+  let fee1 = 0;
+  for (let i = 0; i < users.length; i++) {
+    fee1 += Number(users[i].fees2);
+  }
+  let fee2 = 0;
+  for (let i = 0; i < users.length; i++) {
+    fee2 += Number(users[i].fees3);
+  }
+  let fee3 = 0;
+  for (let i = 0; i < users.length; i++) {
+    fee3 += Number(users[i].fees4);
+  }
   let deptcount = 0;
   for (let i = 0; i < users.length; i++) {
     if (users[i].course === users[i].fullstack) {
       deptcount++;
     }
-    document.getElementById("tbody2").innerHTML = "";
-    let tr = document.createElement("tr");
-    let td1 = document.createElement("td");
-    td1.innerHTML = users[i].fullstack;
-    let td2 = document.createElement("td");
-    td2.innerHTML = deptcount;
-    document.getElementById("tbody2").append(tr);
-    tr.append(td1, td2);
+    let deptcount1 = 0;
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].course === users[i].frontend) {
+        deptcount1++;
+      }
+      let deptcount2 = 0;
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].course === users[i].backend) {
+          deptcount2++;
+        }
+        let deptcount3 = 0;
+        for (let i = 0; i < users.length; i++) {
+          if (users[i].course === users[i].uiux) {
+            deptcount3++;
+          }
+
+          document.getElementById("tbody2").innerHTML = "";
+          let tr = document.createElement("tr");
+          let td1 = document.createElement("td");
+          td1.innerHTML = users[i].fullstack;
+          let td2 = document.createElement("td");
+          td2.innerHTML = deptcount;
+          td3 = document.createElement("td");
+          td3.innerHTML = fee;
+          let tr2 = document.createElement("tr");
+          let td4 = document.createElement("td");
+          td4.innerHTML = users[i].frontend;
+          let td5 = document.createElement("td");
+          td5.innerHTML = deptcount1;
+          let td6 = document.createElement("td");
+          td6.innerHTML = fee1;
+          let tr3 = document.createElement("tr");
+          let td7 = document.createElement("td");
+          td7.innerHTML = users[i].backend;
+          let td8 = document.createElement("td");
+          td8.innerHTML = deptcount2;
+          let td9 = document.createElement("td");
+          td9.innerHTML = fee2;
+          let tr4 = document.createElement("tr");
+          let td10 = document.createElement("td");
+          td10.innerHTML = users[i].uiux;
+          let td11 = document.createElement("td");
+          td11.innerHTML = deptcount3;
+          let td12 = document.createElement("td");
+          td12.innerHTML = fee3;
+          document.getElementById("tbody2").append(tr, tr2, tr3, tr4);
+          tr.append(td1, td2, td3);
+          tr2.append(td4, td5, td6);
+          tr3.append(td7, td8, td9);
+          tr4.append(td10, td11, td12);
+        }
+      }
+    }
   }
 };
 
@@ -78,6 +141,27 @@ const handleData = (e) => {
     fees4: document.getElementById("fees4").value,
     number: document.getElementById("number").value,
   };
+  // if (user.username.length < 2) {
+  //   alert("Username should be at least 2 characters long");
+  //   return;
+  // }
+  // if (user.grid.length != 4) {
+  //   alert("GRID should be 4 characters long");
+  //   return;
+  // }
+  // if (
+  //   user.number.length != 10 ) {
+  //   alert(
+  //     "Phone number should be at least 10 Numbers"
+  //   );
+  //   return;
+  // }
+  // if (!["6","7","8","9"].includes(user.number[0])){
+  //   alert(
+  //     "Phone number should start with 6, 7, 8, or 9"
+  //   );
+  //   return;
+  // }
 
   users.push(user);
   console.log(users);
